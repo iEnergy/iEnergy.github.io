34b30402cb8a634c24e1efb0c5f92c49e6b29b65
alessandraronsini <alessandra.ronsini@gmail.com>
2014-06-20 09:58:41 +0200

    doc pag completed (ita)

--- assets/js/documentation.js
+++ assets/js/documentation.js
@@ -177,13 +177,196 @@ $(document).ready(function(){
    }];
    
    $("#registry-drains-get").JSONView(registry_drains_get_json,{collapsed: true, nl2br: true});
-   $('#collapse-btn').on('click', function() {
-        $('#registry-drains-get').JSONView('collapse');
+   
+   $(".json-expand").click(function(){
+      var id = ($(this).attr('id')).replace('-expand-btn', ''); 
+      console.log(id);
+      $("#"+id).JSONView('expand');
+   });
+   $(".json-collapse").click(function(){
+      var id = ($(this).attr('id')).replace('-collapse-btn', ''); 
+      console.log(id);
+       $("#"+id).JSONView('collapse');
    });
 
-    $('#expand-btn').on('click', function() {
-      $('#registry-drains-get').JSONView('expand');
-    });
-      
+   var registry_drains_post_json = {
+    drain: "DRAIN-TPE",
+    description: "Totale edificio",
+    unit: "W",
+    keepHistory: true,
+    formula: [
+        {
+            drain: "M1-P1",
+            sign: 1
+        }
+    ],
+    aggregationFunction: "AVG",
+    historyFunction: "AVG"
+};
+
+    $("#registry-drains-post").JSONView(registry_drains_post_json,{collapsed: true, nl2br: true});
+    
+   var registry_drains_drain_get_json = {
+    "drain": "DRAIN-TPE",
+    "description": "Totale edificio",
+    "unit": "W",
+    "real": false,
+    "keepHistory": true,
+    "formula": [
+        {
+            "drain": "M1-P1",
+            "sign": 1
+        }
+    ],
+    "aggregationFunction": "AVG",
+    "historyFunction": "AVG"
+};
+    $("#registry-drains-drain-get").JSONView(registry_drains_drain_get_json,{collapsed: true, nl2br: true});
+
+    var measure_instants_drain_get_json = {
+    "drain": "M1-P3",
+    "unit": "W",
+    "value": 20,
+    "time": "2013-12-05T10:12:01+0100"
+};
+
+ $("#measure-instants-drain-get").JSONView(measure_instants_drain_get_json,{collapsed: true, nl2br: true});
+ 
+    var measure_detail_history_drain_get_json = {
+    "drain": "M1-P3",
+    "unit": "W",
+    "queryParam": {
+        "timeAggregation": "MINUTE",
+        "start": "2013-12-05T10:35:16+0100",
+        "end": "2013-12-05T11:35:16+0100"
+    },
+    "measures": [
+        {
+            "value": 15,
+            "time": "2013-12-05T10:36:12+0100"
+        },
+        {
+            "value": 6.67,
+            "time": "2013-12-05T10:37:12+0100"
+        }
+    ]
+};
+$("#measure-detail-history-drain-get").JSONView(measure_detail_history_drain_get_json,{collapsed: true, nl2br: true});
+
+var measure_detail_history_time_get_json = {
+    "start": "2013-12-12T12: 37: 45+0100",
+    "end": "2014-02-20T16: 58: 04+0100"
+};
+
+$("#measure-detail-history-time-get").JSONView(measure_detail_history_time_get_json,{collapsed: true, nl2br: true});
+
+var confort_indicator_get_json = [
+   {
+      "name":"PROVA",
+      "description":"prova prova",
+      "result":-1.0,
+      "function":"AVG",
+      "type":"Humidity"
+   }
+];
+
+$("#confort-indicator-get").JSONView(confort_indicator_get_json,{collapsed: false, nl2br: true});
+
+var confort_indicator_post_json = {
+     "name":"PROVA",
+     "description":"prova prova",
+     "function":"AVG",
+     "type":"Humidity"
+  };
+  
+ 
+  
+  $("#confort-indicator-post").JSONView(confort_indicator_post_json,{collapsed: true, nl2br: true});
+  
+  var confort_indicator_name_get_json = {
+   "name":"PROVA",
+   "description":"prova prova",
+   "result":-1.0,
+   "function":"AVG",
+   "type":"Humidity"
+};
+$("#confort-indicator-name-get").JSONView(confort_indicator_name_get_json,{collapsed: true, nl2br: true});
+  
+  var confort_indicator_name_options_get_json = [
+   {
+      "name":"Wet",
+      "value":1
+   },
+   {
+      "name":"Good",
+      "value":0
+   },
+   {
+      "name":"Dry",
+      "value":-1
+   }
+];
+  
+  $("#confort-indicator-name-options-get").JSONView(confort_indicator_name_options_get_json,{collapsed: true, nl2br: true});
+  
+  
+  var confort_indicator_name_votes_get_json=[
+   {
+      "value":-1,
+      "time":"2013-10-21T15:19:16"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:29"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:30"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:31"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:31"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:31"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:31"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:31"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:32"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:32"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:32"
+   },
+   {
+      "value":-1,
+      "time":"2013-10-21T15:25:32"
+   }
+];
+  $("#confort-indicator-name-votes-get").JSONView(confort_indicator_name_votes_get_json,{collapsed: true, nl2br: true});
+  
+  var confort_indicator_name_votes_post_json ={
+   "value":-1
+};
+  $("#confort-indicator-name-votes-post").JSONView(confort_indicator_name_votes_post_json,{collapsed: true, nl2br: true});
+  
+  
 });
 
